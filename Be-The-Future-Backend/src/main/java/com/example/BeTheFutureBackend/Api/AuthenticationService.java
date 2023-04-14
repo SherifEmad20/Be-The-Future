2b0f0c5b66721d0624e0afc6695923fce4b17faa
Reda Roshdy <rroshdy75@gmail.com>
2023-04-15 01:03:01 +0200

    *

--- Be-The-Future-Backend/src/main/java/com/example/BeTheFutureBackend/Api/AuthenticationService.java
+++ Be-The-Future-Backend/src/main/java/com/example/BeTheFutureBackend/Api/AuthenticationService.java
@@ -10,9 +10,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthenticationService {
 
     private final UserRepository userRepository;
